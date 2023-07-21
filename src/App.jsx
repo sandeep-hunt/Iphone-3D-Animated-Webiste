08e396804ec14db019cfb2c24badb44c1dac42a3
sanddep-dev <sandeep.n062000@gmail.com>
2023-07-21 16:00:42 +0530

    Added Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Jumbotron from "./Components/Jumbotron";
 import SoundSection from "./Components/SoundSection";
 import DisplaySection from "./Components/DisplaySection";
 import WebgiViewer from "./Components/WebgiViewer";
+import Loader from "./Components/Loader";
 
 function App() {
 
   return (
     <div className="App">
+      <Loader />
       <Nav />
       <Jumbotron />
       <SoundSection />
